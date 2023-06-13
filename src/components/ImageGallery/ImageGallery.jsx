a0d58a2a07424dcf490d0eab2bfb6531982dbec7
Tatiana-Tereshchenko <teresenkot33@gmail.com>
2023-06-13 22:43:35 +0300

    hw+

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,29 +4,29 @@ import css from './ImageGallery.module.css';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
 
 export class ImageGallery extends Component {
-  render() {
+    render() {
     const { images, onItemClick } = this.props;
     return (
-      <ul className={css.ImageGallery}>
+        <ul className={css.ImageGallery}>
         {images.map((image, index) => (
-  <ImageGalleryItem
+    <ImageGalleryItem
     key={`image-${image.id}-${index}`}
     imageUrl={image.webformatURL}
     onItemClick={() => onItemClick(image.largeImageURL)}
-  />
+    />
 ))}
-      </ul>
+        </ul>
     );
-  }
+    }
 }
 
 ImageGallery.propTypes = {
-  images: PropTypes.arrayOf(
+    images: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      webformatURL: PropTypes.string.isRequired,
-      largeImageURL: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired,
+        webformatURL: PropTypes.string.isRequired,
+        largeImageURL: PropTypes.string.isRequired,
     })
-  ).isRequired,
-  onItemClick: PropTypes.func.isRequired,
+    ).isRequired,
+    onItemClick: PropTypes.func.isRequired,
 };
